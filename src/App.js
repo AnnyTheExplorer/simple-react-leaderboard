3f6654bb4c173a27b0d32af3b81619584cc35614
Anastasia Agbata <agbataanastasia@gmail.com>
2023-02-20 01:30:03 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React from 'react'
+import "./App.css";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Leaderboard</h2>
+        <Home />
+      </header>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className="d-flex home mt-3">
+      <table class="table w-100 text-black">
+        <thead>
+          <tr className="description">
+            <th scope="col">#</th>
+            <th scope="col">Interns</th>
+            <th scope="col">Total Score</th>
+            <th scope="col">Track</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="position">01</td>
+            <td className="details">Anny Agbata</td>
+            <td className="details">10,025</td>
+            <td className="details">Web3</td>
+          </tr>
+          <tr>
+            <td className="position">02</td>
+            <td className="details">Ngozi Mike</td>
+            <td className="details">9,980</td>
+            <td className="details">Backend</td>
+          </tr>
+          <tr>
+            <td className="position">03</td>
+            <td className="details">Blessing Udu</td>
+            <td className="details">9,979</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td className="position">04</td>
+            <td className="details">Mikke Micheal</td>
+            <td className="details">9,970</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td className="position">05</td>
+            <td className="details">Kunle Ajibade</td>
+            <td className="details">9,880</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td className="position">06</td>
+            <td className="details">Rose Ani</td>
+            <td className="details">9,800</td>
+            <td className="details">Product Design</td>
+          </tr>
+          <tr>
+            <td className="position">07</td>
+            <td className="details">Mikke Maxwell</td>
+            <td className="details">9,770</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>08</td>
+            <td className="details">Obi Okafor</td>
+            <td className="details">9,760</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>09</td>
+            <td className="details">John Blessed</td>
+            <td className="details">9, 689</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td className="details">Nuella Chris</td>
+            <td className="details">9,600</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>11</td>
+            <td className="details">Moe Melon</td>
+            <td className="details">9,550</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>12</td>
+            <td className="details">Nae Ajibade</td>
+            <td className="details">9,470</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>13</td>
+            <td className="details">Ngozi Okafor</td>
+            <td className="details">9,440</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>14</td>
+            <td className="details">Emmanuel Okeke</td>
+            <td className="details">9,439</td>
+            <td className="details">Web3</td>
+          </tr>
+          <tr>
+            <td>15</td>
+            <td className="details">Nate Nwachukwu</td>
+            <td className="details">9,320</td>
+            <td className="details">Product Design</td>
+          </tr>
+          <tr>
+            <td>16</td>
+            <td className="details">Kunle John</td>
+            <td className="details">9,318</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>17</td>
+            <td className="details">Ada Obi</td>
+            <td className="details">9,312</td>
+            <td className="details">Backend</td>
+          </tr>
+          <tr>
+            <td>18</td>
+            <td className="details">Blue Lala</td>
+            <td className="details">9,310</td>
+            <td className="details">Web3</td>
+          </tr>
+          <tr>
+            <td>19</td>
+            <td className="details">Princess Akpan</td>
+            <td className="details">9,309</td>
+            <td className="details">Frontend</td>
+          </tr>
+          <tr>
+            <td>20</td>
+            <td className="details">Queen Fatima</td>
+            <td className="details">9,970</td>
+            <td className="details">Product Design</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Home
